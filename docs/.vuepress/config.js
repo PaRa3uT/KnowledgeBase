da2e209d470966bb9f2f61a5be704012222a6c33
Tarasov Artur <para3ut.74@gmail.com>
2021-09-16 13:48:53 +0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,9 @@ module.exports = {
     themeConfig: {
         navbar: [
             {text: 'Главная', link: '/'},
+            {text: 'Dev', children: [
+                {text: 'Git', link: '/dev/git'}
+            ]},
             {text: 'OS', children: [
                 {text: 'Linux', children: [
                     {text: 'Common', link: '/linux/common'}
